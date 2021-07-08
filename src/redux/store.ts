f2b5b4aef5d5bc721a90feb5fb6607170c6aa313
mat2718 <matthewterry68.mt@gmail.com>
2021-07-08 13:09:08 -0400

    add redux extension for dev ops

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,27 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import {reducers} from './reducers/index';
 import thunk from 'redux-thunk'
 
-const store = createStore(
-    reducers,
-    {},
-    applyMiddleware(thunk)
-)
+const w:any = window;
+const composeEnhancers =
+  typeof window === 'object' &&
+  w.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    w.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+const store = createStore(reducers,{}, enhancer);
+
+
+// const store = createStore(
+//     reducers,
+//     {},
+//     applyMiddleware(thunk) + w.__REDUX_DEVTOOLS_EXTENSION__ && w.__REDUX_DEVTOOLS_EXTENSION__(),
+// )
 
 export default store;
+
