d0c57b5fd4960adc2834aa5b171d1dc12a64823e
Brandon Kirsch <brandon.kirsch.4@gmail.com>
2021-07-08 11:53:34 -0700

    Remove React Native
    Removes all traces of React Native from the user profile page, in favor of Reactstrap

--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -1,25 +1,13 @@
 import React, { useState}  from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
-import InputField from "./UserInputField";
+import { Form, FormGroup, Label, Input, FormText, Button } from 'reactstrap';
+import { IUser } from "../../models/userModel";
 
-
-
-const UserDataForm = () => {
+const UserDataForm = (user:IUser) => {
     const [editable, toggleEditing] = useState(false);
     const [buttonText, setButtonText] = useState("Edit");
-    const [userData, setUserData] = useState({
-        Email: "",
-        FirstName: "",
-        LastName: "",
-        BirthDate: "",
-        PhoneNumber: "",
-        NickName: "",
-        PreferredName: "",
-        Profile: "",
-    });
+    const [userData, setUserData] = useState(user);
 
-    const submitHandle = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const submitHandle = () => {
         toggleEditing(() => {return !editable});
         if (!editable) {
             setButtonText("Save");
@@ -38,40 +26,39 @@ const UserDataForm = () => {
     }
   
     return (
-        <form onSubmit={e => {submitHandle(e)}}>
-        <View
-            style={styles.outerForm}>
-                <View style={{flexDirection: "row"}}>
-                    <InputField name="Email" label="Profile" inText={userData.Email}/>
-                    <View style={styles.buttonContainer}><input type="submit" value={buttonText}/></View>
-                </View>
-                <View style={{flexDirection: "row"}}>
-                    <InputField inText={userData.FirstName} label="First Name" name="FirstName" isEditing={editable} event={manageEdits}/>   
-                    <InputField inText={userData.LastName} label="Last Name" name="LastName" isEditing={editable} event={manageEdits}/>
-                </View>
-                <InputField inText={userData.BirthDate} label="Birthday" name="BirthDate" isEditing={editable} event={manageEdits}/>
-                <InputField inText={userData.PhoneNumber} label="Phone Number" name="PhoneNumber" isEditing={editable} event={manageEdits}/>
-                <InputField inText={userData.NickName} label="Nickname" name="NickName" isEditing={editable} event={manageEdits}/>
-                <InputField inText={userData.PreferredName} label="Preferred Name" name="PreferredName" isEditing={editable} event={manageEdits}/>
-                <InputField inText={userData.Profile} label="Profile" name="Profile" isEditing={editable} event={manageEdits}/>
-            </View>
-        </form>
+        <Form style={{backgroundColor: "#30315A"}}>
+            <FormGroup>
+                <Label for="email" style={{color: "#ccc"}}>Profile Page</Label>
+                <Input plaintext value={userData.userName} disabled/>
+                <Button onClick={submitHandle}>{buttonText}</Button>
+            </FormGroup>
+            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                <Label for="name" style={{color: "#ccc"}}>Name</Label>
+                <Input name="FirstName" placeholder="First" value={userData.firstName} onChange={manageEdits} disabled={!editable} />
+                <Input name="LastName" placeholder="Last" value={userData.lastName} onChange={manageEdits} disabled={!editable} />
+            </FormGroup>
+            <FormGroup>
+                <Label style={{color: "#ccc"}}>Birthday</Label>
+                <Input type="date" name="BirthDate" value={userData.birthDate} onChange={manageEdits} disabled={!editable}/>
+            </FormGroup>
+            <FormGroup>
+                <Label style={{color: "#ccc"}}>Phone Number</Label>
+                <Input name="PhoneNumber" value={userData.phoneNumber} onChange={manageEdits} disabled={!editable}/>
+            </FormGroup>
+            <FormGroup>
+                <Label style={{color: "#ccc"}}>Nickname</Label>
+                <Input name="NickName" value={userData.nickName} onChange={manageEdits} disabled={!editable}/>
+            </FormGroup>
+            <FormGroup>
+                <Label style={{color: "#ccc"}}>Preferred Name</Label>
+                <Input name="PreferredName" value={userData.publicName} onChange={manageEdits} disabled={!editable}/>
+            </FormGroup>
+            <FormGroup>
+                <Label style={{color: "#ccc"}}>Profile</Label>
+                <Input type="textarea" name="Profile" value={userData.profile} onChange={manageEdits} disabled={!editable}/>
+            </FormGroup>
+        </Form>
     );
   };
-
-  const styles = StyleSheet.create({
-    outerForm: {
-        flexDirection: "column",
-        height: 600,
-        width: 400,
-        padding: 20,
-        backgroundColor: "#30315A",
-    },
-    buttonContainer: {
-        flex: 0.2,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-});
 
 export default UserDataForm;
