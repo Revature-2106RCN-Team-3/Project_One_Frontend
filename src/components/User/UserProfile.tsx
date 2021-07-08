e30b8ab6305183558532d25295e4e8b55af71adc
Brandon Kirsch <brandon.kirsch.4@gmail.com>
2021-07-08 12:41:39 -0700

    Make it pretty
    Implements container tags, both for standalone structure and for CSS utilization

--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -1,12 +1,15 @@
-import React, { useState}  from "react";
-import { Form, FormGroup, Label, Input, FormText, Button } from 'reactstrap';
+import { useState}  from "react";
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Container, Row, Col } from "reactstrap";
 import { IUser } from "../../models/userModel";
 
 const UserDataForm = (user:IUser) => {
     const [editable, toggleEditing] = useState(false);
     const [buttonText, setButtonText] = useState("Edit");
+    // Passes user data in as an object.
     const [userData, setUserData] = useState(user);
 
+    // Activated upon pressing the save/edit button
     const submitHandle = () => {
         toggleEditing(() => {return !editable});
         if (!editable) {
@@ -18,6 +21,7 @@ const UserDataForm = (user:IUser) => {
         }
     }
 
+    // Called every time any field is changed
     const manageEdits = (e: any) => {
         const target = e.target;
         setUserData({
@@ -26,38 +30,42 @@ const UserDataForm = (user:IUser) => {
     }
   
     return (
-        <Form style={{backgroundColor: "#30315A"}}>
-            <FormGroup>
-                <Label for="email" style={{color: "#ccc"}}>Profile Page</Label>
-                <Input plaintext value={userData.userName} disabled/>
-                <Button onClick={submitHandle}>{buttonText}</Button>
-            </FormGroup>
-            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                <Label for="name" style={{color: "#ccc"}}>Name</Label>
-                <Input name="FirstName" placeholder="First" value={userData.firstName} onChange={manageEdits} disabled={!editable} />
-                <Input name="LastName" placeholder="Last" value={userData.lastName} onChange={manageEdits} disabled={!editable} />
-            </FormGroup>
-            <FormGroup>
-                <Label style={{color: "#ccc"}}>Birthday</Label>
-                <Input type="date" name="BirthDate" value={userData.birthDate} onChange={manageEdits} disabled={!editable}/>
-            </FormGroup>
-            <FormGroup>
-                <Label style={{color: "#ccc"}}>Phone Number</Label>
-                <Input name="PhoneNumber" value={userData.phoneNumber} onChange={manageEdits} disabled={!editable}/>
-            </FormGroup>
-            <FormGroup>
-                <Label style={{color: "#ccc"}}>Nickname</Label>
-                <Input name="NickName" value={userData.nickName} onChange={manageEdits} disabled={!editable}/>
-            </FormGroup>
-            <FormGroup>
-                <Label style={{color: "#ccc"}}>Preferred Name</Label>
-                <Input name="PreferredName" value={userData.publicName} onChange={manageEdits} disabled={!editable}/>
-            </FormGroup>
-            <FormGroup>
-                <Label style={{color: "#ccc"}}>Profile</Label>
-                <Input type="textarea" name="Profile" value={userData.profile} onChange={manageEdits} disabled={!editable}/>
-            </FormGroup>
+        <Container>
+        <Form>
+            <Row fluid="true">
+                <Col xs="2"><Label for="email" style={{color: "#ccc"}}>Profile Page</Label></Col>
+                <Col><Input plaintext value={userData.userName} disabled/></Col>
+                <Col xs="1"><Button onClick={submitHandle}>{buttonText}</Button></Col>
+            </Row>
+            <Row>
+                <Col><Label for="name" style={{color: "#ccc"}}>Name</Label></Col>
+            </Row><Row>
+                <Col><Input name="FirstName" placeholder="First" value={userData.firstName} onChange={manageEdits} disabled={!editable} /></Col>
+                <Col><Input name="LastName" placeholder="Last" value={userData.lastName} onChange={manageEdits} disabled={!editable} /></Col>
+            </Row>
+            <Row>
+                <Col><Label style={{color: "#ccc"}}>Birthday</Label></Col>
+            </Row><Row>
+                <Col><Input type="date" name="BirthDate" value={userData.birthDate} onChange={manageEdits} disabled={!editable}/></Col>
+            </Row><Row>
+                <Col><Label style={{color: "#ccc"}}>Phone Number</Label></Col>
+            </Row><Row>
+                <Col><Input name="PhoneNumber" value={userData.phoneNumber} onChange={manageEdits} disabled={!editable}/></Col>
+            </Row><Row>
+                <Col><Label style={{color: "#ccc"}}>Nickname</Label></Col>
+            </Row><Row>
+                <Col><Input name="NickName" value={userData.nickName} onChange={manageEdits} disabled={!editable}/></Col>
+            </Row><Row>
+                <Col><Label style={{color: "#ccc"}}>Preferred Name</Label></Col>
+            </Row><Row>
+                <Col><Input name="PreferredName" value={userData.publicName} onChange={manageEdits} disabled={!editable}/></Col>
+            </Row><Row>
+                <Col><Label style={{color: "#ccc"}}>Profile</Label></Col>
+            </Row><Row>
+                <Col><Input type="textarea" name="Profile" value={userData.profile} onChange={manageEdits} disabled={!editable}/></Col>
+            </Row>
         </Form>
+        </Container>
     );
   };
 
