f23663784ff3f6bc309f0f64915bf6c2e482d24d
mat2718 <matthewterry68.mt@gmail.com>
2021-07-09 04:23:16 -0400

    minor updates

--- src/redux/action-types/index.ts
+++ src/redux/action-types/index.ts
@@ -1,9 +1,10 @@
 export enum ActionType {
     REGISTER_START = 'register-start',
     REGISTER_SUCCESS = 'register-success',
-    LOGOUT_START= 'logout-start',
+    LOGOUT_START = 'logout-start',
     SET_USERNAME = 'set-username',
     SET_LOADING = 'set-password',
     AUTHENTICATED = 'authenticated',
-    IS_ERROR = 'is-error'
+    IS_ERROR = 'is-error',
+    SET_AUTH_LOADING = "SET_AUTH_LOADING"
 }
