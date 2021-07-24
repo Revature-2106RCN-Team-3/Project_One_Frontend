f9e37ac2bf366fca7972e7079f5a0d2986cf2b7f
mat2718 <matthewterry68.mt@gmail.com>
2021-07-24 16:01:36 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import './App.css';
-import { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { PublicRoute, Protected } from './routers/index';
-import { useDispatch } from 'react-redux';
-import { checkAuthSess } from './api';
-import { loginSuccess } from './redux/actions/logRegAction';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Navbar from './components/Navigation/Navigation';
